a59042891d6cb1babc1b15922d7fb9500c0b0026
bestemorgul <beste.morgul@gmail.com>
2024-06-30 15:03:18 +0300

    yenidw

--- tests/test_deneme.py
+++ tests/test_deneme.py
@@ -4,7 +4,7 @@ class TestCheckSmartJourneySelectChannel():
     partner_name = "qaautomation1"
     settings.env_variables[SettingKeys.PARTNER_NAME] = "qaautomation1"
 
-    settings.env_variables[SettingKeys.PARTNER_PANEL_URL] = "https://qaautomation1.inone.useinsider.com/"
+    settings.env_variables[SettingKeys.PARTNER_PANEL_URL] = "https://A.inone.useinsider.com/"
     partner_url = "https://qaautomation1.inone.useinsider.com/"
 
 
