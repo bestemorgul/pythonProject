06104c5a89583ecf495a8e9e9a546905b3249563
bestemorgul <beste.morgul@gmail.com>
2024-06-30 19:04:11 +0300

    yenidw

--- tests/test_deneme.py
+++ tests/test_deneme.py
@@ -22,7 +22,7 @@ class TestCheckSmartJourneySelectChannel():
     second_selected_channel_name = "Email"
 
     def setUp(self):
-        settings.env_variables[SettingKeys.PARTNER_NAME] = "ABC"
+        settings.env_variables[SettingKeys.PARTNER_PANEL_URL] = "https://partner.inone.useinsider.com/"
         self.campaign_name = self.generate_campaign_name()
         self.user_id = self.create_random_string(7)
 
