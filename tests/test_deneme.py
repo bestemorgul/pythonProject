71037ed69057137ce8472edba375f7f1ec1fd85f
bestemorgul <beste.morgul@gmail.com>
2024-06-30 19:01:46 +0300

    yenidw

--- tests/test_deneme.py
+++ tests/test_deneme.py
@@ -22,6 +22,7 @@ class TestCheckSmartJourneySelectChannel():
     second_selected_channel_name = "Email"
 
     def setUp(self):
+        settings.env_variables[SettingKeys.PARTNER_NAME] = "qaautomation1"
         self.campaign_name = self.generate_campaign_name()
         self.user_id = self.create_random_string(7)
 
