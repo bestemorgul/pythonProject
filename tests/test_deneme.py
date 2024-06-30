94e21fba65a138f35bde8969f73e04a095993899
bestemorgul <beste.morgul@gmail.com>
2024-06-30 19:09:14 +0300

    yenidw

--- tests/test_deneme.py
+++ tests/test_deneme.py
@@ -22,7 +22,7 @@ class TestCheckSmartJourneySelectChannel():
     second_selected_channel_name = "Email"
 
     def setUp(self):
-        partner_url = "https://partner.inone.useinsider.com/"
+        partner_url = "https://hellokitty.inone.useinsider.com/"
         self.campaign_name = self.generate_campaign_name()
         self.user_id = self.create_random_string(7)
 
